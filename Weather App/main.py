183b97d548c114d94f3636586051ef2ce7bcadb1
JRM-code <83676722+JRM-code@users.noreply.github.com>
2025-11-23 13:47:36 +1000

    Add files via upload

--- Weather App/main.py
+++ Weather App/main.py
@@ -6,26 +6,50 @@ st.set_page_config(page_title='Weather App', layout='wide')
 st.title("Weather App")
 st.sidebar.title("Location & Forecast")
 
+# --- GET LOCATION AND SEND LAT LON TO GET DATA -- #
+# The dictionary of places with latitude and longitude
+PLACES = {
+    "Brisbane": {"lat": -27.47, "lon": 153.02},
+    "Sydney": {"lat": -33.8688, "lon": 151.2093},
+    "Melbourne": {"lat": -37.81, "lon": 144.96},
+    "Adelaide": {"lat": -34.93, "lon": 138.60},
+    "Darwin": {"lat": -12.46, "lon": 130.84},
+    "Perth": {"lat": -31.95, "lon": 115.86},
+    "Hobart": {"lat": -42.88, "lon": 147.33},
+    "Canberra": {"lat": -35.28, "lon": 149.13}
+}
+
+# --- SELECTBOX FOR LOCATION --- #
+place_name = st.sidebar.selectbox("Choose your location", options=PLACES)
+
+
+def lat_lon():
+    return PLACES.keys()
+lat_lon()
+
+def get_coords(place_name):
+    coords = PLACES.get(place_name, {})
+    #st.write(coords)
+    return coords 
+coords = get_coords(place_name)
+
 # --- GET DATA --- #
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-	"latitude": 27.47,
-	"longitude": 153.02,
-	"hourly": "temperature_2m","current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "pressure_msl", "wind_direction_10m", "wind_speed_10m", "wind_gusts_10m"],
+	"latitude": coords["lat"],
+	"longitude": coords["lon"],
+	"hourly": "temperature_2m","current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "pressure_msl", "wind_direction_10m", "wind_speed_10m", "wind_gusts_10m", "rain", "is_day"],
 	"timezone": "Australia/Brisbane"
 }
 headers = {
     "User-Agent": "MyWeatherApp/1.0 (example.com)"
 }
 
-def get_weather() -> str:
+def get_weather():
     response = requests.get(url, params=params, headers=headers)
     data = response.json()
     return data
 data = get_weather()
-
-# --- SELECTBOX FOR LOCATION --- #
-location = st.sidebar.selectbox("Choose your location", options=['Brisbane', 'New York'])
 
 # --- SELECTBOX FOR FORECAST TIMEFRAME --- #
 st.sidebar.selectbox("Choose your forecast", options=['Today', '3-Days', '5-Days', '7-Days'])
